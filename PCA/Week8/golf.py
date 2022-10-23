1f12f9d5f5e1a6ffff1e3cc8cf003fa3a230ffa6
unknown <chissanuoak2@gmail.com>
2022-10-23 23:41:58 +0700

    add from labtop

--- PCA/Week8/golf.py
+++ PCA/Week8/golf.py
@@ -3,94 +3,36 @@ class Node:
         self.data = initdata
         self.next = None
         self.prev = None
-
-    def getData(self):
-        return self.data
-
-    def getNext(self):
-        return self.next
-    
-    def getPrev(self):
-        return self.prev
-
-    def setData(self,newData):
-        self.data = newData
-
-    def setNext(self,newNext):
-        self.next = newNext
-    
-    def setPrev(self,newPrev):
-        self.prev = newPrev
         
-class UnorderedList:
+class DoubleLinkedList:
     def __init__(self):
         self.head = None
-    
-    def isEmpty(self):
-        return self.head == None
-    
-    def add(self,item):
-        newNode = Node(item)
+        
+    def push(self,data):
+        newNode = Node(data)
         newNode.next = self.head
         if self.head is not None:
             self.head.prev = newNode
-        self.head = newNode
+            self.head = newNode
     
-    def size(self):
-        current = self.head
-        count = 0
-        while current != None:
-            count = count + 1
-            current = current.getNext()
-        return count
-
-    def getList(self):
-        current = self.head
-        tempList = []
-        while current != None:
-            tempList.append(current.getData())
-            current = current.getNext()
-        tempList.reverse()
-        return tempList
-
-    def search(self,item):
-        current = self.head
-        found = False
-        while current != None and not found:
-            if current.getData() == item:
-                found = True
-            else:
-                current = current.getNext()
-        return found
+    def insertAt(self,prevNode,data):
+        newNode = Node(data)
+        newNode.next = prevNode.next
+        prevNode.next = newNode
+        newNode.prev = prevNode
+        if newNode.next is not None:
+            newNode.next.prev = newNode
+        
     
-    def remove(self,item):
-        current = self.head
-        previous = None
-        found = False
-        while not found:
-            if current.getData() == item:
-                found = True
-            else:
-                previous = current
-                current = current.getNext()
-        if previous == None:
-            self.head = current.getNext()
-        else:
-            previous.setNext(current.getNext())
-
-    def printNode(self):
-        current = self.head
-        while current != None:
-            print(current.getData())
-            current = current.getNext()
+    
 
 
 def main():
-    myList = UnorderedList()
-    myList.add("Oak")
-    myList.add("John")
-    myList.add("Ruj")
-    myList.printNode()
+    myList = DoubleLinkedList()
+    myList.push("Oak")
+    myList.push("Ruj")
+    myList.push("4th")
+    myList.push("Ton")
     #print(myList.getList())
     
 main()

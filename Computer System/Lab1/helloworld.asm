e1c73c6bc9ffbc4c7cd77cad2c2866a5cc2c538d
Chissanu <chissanuoak2@gmail.com>
2023-01-24 11:54:50 +0700

    Add cognitive labs

---
+++ Computer System/Lab1/helloworld.asm
@@ -0,0 +1,8 @@
+# PROGRAM: Hello, World!
+.data # Data declaration section
+out_string: .asciiz "\nHello, Worldsss!\n"
+.text # Assembly language instructions
+main: # Start of code section
+    li $v0, 4 # system call code for printing string = 4
+    la $a0, out_string # load address of string to be printed into $a0
+    syscall
